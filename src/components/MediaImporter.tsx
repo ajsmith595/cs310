87ee466877eb6f9e43a74de68b417c69fdafa844
Adam Smith <ajsmith595@hotmail.co.uk>
2021-10-15 18:09:50 +0100

    Initial commit

---
+++ src/components/MediaImporter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+interface Props {
+	// props
+}
+
+interface State {
+	// state
+}
+
+class MediaImporter extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+	}
+
+	render() {
+		return null;
+	}
+
+}
+
+export default MediaImporter;
