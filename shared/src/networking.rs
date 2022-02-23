c0d2b5ffd4c6c0cede033edddf4037eb6889efba
Adam Smith <ajsmith595@hotmail.co.uk>
2022-02-23 14:47:50 +0000

    Some changes to networking and removed some unnecessary code

--- shared/src/networking.rs
+++ shared/src/networking.rs
@@ -21,13 +21,23 @@ enum_from_primitive! {
         EndFile,
         NewChunk,
         AllChunksGenerated,
+        CreateFile,
         GetFileID,
-        CompositedClipLength
+        CompositedClipLength,
+        Checksum,
+        ChecksumOk,
+        ChecksumError,
     }
 }
 
 pub fn connect_to_server() -> Result<TcpStream, Error> {
     TcpStream::connect(format!("{}:{}", SERVER_HOST, SERVER_PORT))
+}
+
+pub fn send_as_file(stream: &mut TcpStream, file_data: &[u8]) {
+    let length = file_data.len().to_ne_bytes();
+    send_data(stream, &length).unwrap(); // send the file length
+    send_data(stream, file_data).unwrap();
 }
 
 pub fn send_file(stream: &mut TcpStream, file: &mut File) {
@@ -54,6 +64,24 @@ where
     let bytes = file_length.to_ne_bytes();
     send_data(stream, &bytes).unwrap(); // send the file length
     std::io::copy(&mut reader, stream).unwrap();
+}
+
+pub fn receive_file_as_bytes(stream: &mut TcpStream) -> Vec<u8> {
+    let file_length = receive_data(stream, 8).unwrap();
+    let mut file_length_bytes = [0 as u8; 8];
+    file_length_bytes.copy_from_slice(&file_length[0..8]);
+    let file_length = u64::from_ne_bytes(file_length_bytes);
+    let mut data = stream.take(file_length);
+
+    if file_length > (usize::MAX as u64) {
+        panic!("Cannot handle file length greater than max usize value!");
+    }
+    let file_length = file_length as usize;
+
+    let mut buffer = vec![0; file_length];
+    data.read(&mut buffer).unwrap();
+
+    buffer
 }
 
 pub fn receive_file(stream: &mut TcpStream, output_file: &mut File) {
